3cdefd669ab577d24d388426a8a881a9ee927f7a
owias azizi <owaisazizi360@gmail.com>
2025-06-26 10:53:37 +0430

    first interface version

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = loader.load();
+
+        // Replace with your controller package path if needed
+        Controller controller = loader.getController();
+        controller.setMainWindow(primaryStage);
+
+        primaryStage.setTitle("Hello world");
+        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
